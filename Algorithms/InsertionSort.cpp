cf742db826f1dcac409a0a592ab5e41db7e465c5
Tiago Ramalho <49025598+Tiago4k@users.noreply.github.com>
2019-05-16 12:15:59 +0100

    Added comments

--- Algorithms/InsertionSort.cpp
+++ Algorithms/InsertionSort.cpp
@@ -10,6 +10,9 @@ void insertionSort(int arr[], int n){
         int item = arr[i];
         int j = i-1;
         
+        /*Move elements of arr[0...i-1}, that are
+        greater than item, to one position ahead of
+        their current position.*/  
         while(j >= 0 && arr[j] > item){
             arr[j + 1] = arr[j];
             j--;
