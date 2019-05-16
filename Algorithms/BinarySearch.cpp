231c8e939c7fce612489219451e75b615ac449fe
Tiago Ramalho <49025598+Tiago4k@users.noreply.github.com>
2019-05-16 12:29:23 +0100

    Added Comments

--- Algorithms/BinarySearch.cpp
+++ Algorithms/BinarySearch.cpp
@@ -3,23 +3,24 @@
 #include <chrono> 
 using namespace std;
 
+/* It returns location of x in given array 
+arr[l..r] is present, otherwise -1 */
 int recursiveBinarySearch(int arr[], int left, int right, int target){
     
     if(right >= left){
-        
         int mid = left + (right - left)/2;
         
+        // If the element is present at the middle, return mid 
         if(arr[mid] == target) return mid;
         
+        // If element is smaller than mid, then it can only be present in left half 
         if(target < arr[mid]) return recursiveBinarySearch(arr, left, mid-1, target);
         
+        // Else the element can only be present in right half 
         return recursiveBinarySearch(arr, mid+1, right, target);
     }
-    
     return false; 
-
 }
-
 
 int iterativeBinarySearch(int arr[], int left, int right, int target){
     
@@ -27,10 +28,14 @@ int iterativeBinarySearch(int arr[], int left, int right, int target){
         
         int mid = left + (right - left)/2;
         
+        // Check if target is present in mid
         if(arr[mid] == target) return mid;
         
+        // If the target is greater than the mid, ignore left half
         if(target > arr[mid]){
             left = mid + 1;
+        
+        // If targer is less than the mid, ignore right half
         }else{
             right = mid - 1;
         }
